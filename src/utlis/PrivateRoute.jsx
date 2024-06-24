9f59e9111a759240c18570ac0de0c775c3ab7a25
rockyman109 <82431601+rockyman109@users.noreply.github.com>
2024-06-24 14:39:41 +0530

    Update PrivateRoute.jsx

--- src/utlis/PrivateRoute.jsx
+++ src/utlis/PrivateRoute.jsx
@@ -6,7 +6,8 @@ function PrivateRoute({ children }) {
   const isAuthenticated = useSelector((state) => state?.user?.isAuthenticated);
   const token = sessionStorage.getItem('token');
   // console.log("isAuthenticated", isAuthenticated);
-  return isAuthenticated && token ? children : <Navigate to="/login" replace />;
+  // return isAuthenticated && token ? children : <Navigate to="/login" replace />;
+  return  token ? children : <Navigate to="/login" replace />;
 }
 
 export default PrivateRoute;
